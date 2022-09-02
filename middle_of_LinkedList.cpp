66fd0fd806cf384dc855264387c8985ece137bc4
taruneluri <taruneluri2106@gmail.com>
2022-09-02 10:17:52 +0530

    reverse of linked list

---
+++ middle_of_LinkedList.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == NULL)
+        {
+            return head;
+        }
+        ListNode* first=head;
+        ListNode* second=head;
+        while(second != NULL and second->next != NULL )
+        {
+            first=first->next;
+            second=second->next->next;
+        }
+        return first;
+    }
+};
